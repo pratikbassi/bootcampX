7d8fb5f8889ef1d39c8178bb36a08c3ed3df2751
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-03-03 17:40:22 +0000

    added two seeds and a migration that creates another two tables

---
+++ migrations/teachers_assistance.sql
@@ -0,0 +1,19 @@
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  is_active BOOLEAN
+);
+
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) on DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) on DELETE CASCADE,
+  created_at DATE,
+  started_at DATE,
+  completed_at DATE,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
